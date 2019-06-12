88960c38f5cec26ab302c0c7468fb25699ee0b54
Owlree <owlree128@gmail.com>
2019-06-12 15:22:40 +0000

    Update data

--- data/generate.py
+++ data/generate.py
@@ -4,42 +4,37 @@ our CloudSearch instance.
 """
 
 from typing import List, TextIO, TypeVar
-import csv
+import csv, json
 
 
 def main():
-    first_row:List[str] = [
-        'city',
-        'city_ascii',
-        'latlon',
-        'country',
-        'iso2',
-        'iso3',
-        'population']
-
     csv_file: TextIO
     csv_file_export: TextIO
-    with open('worldcities.csv') as csv_file:
-        with open('worldcities-export.csv', 'w') as csv_file_export:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            csv_writer = csv.writer(csv_file_export, delimiter=',')
+    with open('data.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        
+        column_names = next(csv_reader)
+        
+        places = []
 
-            csv_writer.writerow(first_row)
-
-            next(csv_reader) # ignore column names
-
-            for row in csv_reader:
-                latlon: str = f'{str(row[2])},{str(row[3])}'
-                csv_item_type = TypeVar('csv_item_type', int, str)
-                city: List[csv_item_type] = [
-                    row[0],  # city
-                    row[1],  # city_ascii
-                    latlon,  # latlon
-                    row[4],  # country
-                    row[5],  # iso2
-                    row[6],  # iso3
-                    row[9]]  # population
-                csv_writer.writerow(city)
+        for row in csv_reader:
+            row_dict = {}
+            i = 0
+            n = len(row)
+            d = {}
+            while i < n:
+                if len(row[i]) > 0:
+                    d[column_names[i]] = row[i]
+                i += 1
+            places.append(d)
+            
+        for place in places:
+            if 'Phone' in place:
+                place['Phone'] = place['Phone'].replace(' ', '')
+            place['Coordinates'] = [float(s) for s in place['Coordinates'].split(',')]
+            place['Number'] = int(place['Number'])
+        
+        print(json.dumps({'places': places}, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
